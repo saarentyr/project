535d6ea1ab59d7ce0f9647f1758346d5cd60b42b
tyr <grocaut_thierry@yahoo.fr>
2018-11-20 11:45:31 +0100

    dev commit

--- src/First.java
+++ src/First.java
@@ -2,5 +2,6 @@
 public class First{
 	public static void main(String[] args){
 		System.out.println("This is the first !"");
+		System.out.println("...but from the second commit.");
 	}
 }
---
+++ src/Second.java
@@ -0,0 +1,7 @@
+
+public class Second{
+	public static void main(String[] args){
+		System.out.println("This is the second !"");
+		System.out.println("...from the second commit.");
+	}
+}
